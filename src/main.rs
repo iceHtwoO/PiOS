763323af682312220191245f4de20a387694a160
Alexander Neuhäuser <alexander.neuhaeuser@tuta.io>
2025-05-24 21:18:41 +0200

    Step1

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,18 @@
-fn main() {
+#![no_main]
+#![no_std]
+
+use core::{arch::asm, panic::PanicInfo};
+
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text._start")]
+pub unsafe extern "C" fn _start() {
+    // Set the stack pointer
+    asm!("ldr x0, =0x8004000", "mov sp, x0");
+    loop{}
 }
